ace8ee96e33d2452a03de57b9993215621b58d66
POASYTLSXXX1\Mouncef SADOQ <mouncefsadoq@gmail.com>
2019-01-09 17:29:58 +0100

    Add bearer token in local storage for authentification

--- examples/demo/src/authProvider.js
+++ examples/demo/src/authProvider.js
@@ -2,22 +2,22 @@ import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_CHECK, AUTH_ERROR } from 'react-admin';
 
 export default (type, params) => {
     if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
+        const { username, password } = params;
+        const request = new Request('http://localhost:8080/api/v1/auth/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        })
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                localStorage.setItem('token', token);
+            });
     }
-    if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('username');
-        return Promise.resolve();
-    }
-    if (type === AUTH_ERROR) {
-        return Promise.resolve();
-    }
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem('username')
-            ? Promise.resolve()
-            : Promise.reject();
-    }
-    return Promise.reject('Unkown method');
+    return Promise.resolve();
 };
